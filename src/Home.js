8caa97752ae9e72028b15390eb9af3f236f8f8eb
ET <431259@cognizant.com>
2017-11-14 11:28:57 +0530

    changes

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends React.Component {
+ render() {
+   return (
+     <div>
+       <h2>HELLO</h2>
+       <p></p>
+
+       <p></p>
+     </div>
+   );
+ }
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import Main from "./Main";
  
 ReactDOM.render(
-  <Main/>, 
+  <Main/>,
   document.getElementById("root")
 );
